2e8b02f4f4bab28a39ec0742be0bb77cc60c0c18
rubylovesbread <admin@batatapenguin.com>
2024-08-08 06:36:33 +0000

    add files

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
-import main
+from flask import Flask, render_template
+from main import main
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     main()
     print('success')
-    return 'Hello, World!'
+    return render_template('index.html')
+
+
+if __name__ == ' __main__':
+    app.debug = True
+    app.run()
--- main.py
+++ main.py
@@ -8,39 +8,44 @@ from moviepy.video.fx import loop, resize
 
 from helpers import cat, upload
 
+COUNTER = 3
+
 
 def main():
-    clip = ColorClip(size=(1080, 1920), color=[255, 255, 255], duration=10)
-
-    audioClip = AudioFileClip("audio/Cat Circus - Doug Maxwell.mp3").set_duration(clip.duration)
-    audioClip = volumex(audioClip, 0.1)
-
-    #gif = cat.get_cat_gif()
-    gif = 'temp/img.gif'
-    gifClip = VideoFileClip(gif).set_position(('center', 350)).set_duration(clip.duration)
-    loopGifClip = loop.loop(gifClip, duration=clip.duration)
-    bigGifClip = resize.resize(loopGifClip, (gifClip.w * 2, gifClip.h * 2))
-
-    textClip = TextClip(txt='Cat Facts #1', font="Amiri-Bold", fontsize=150, color="black").set_duration(clip.duration)
-    textClip = textClip.set_position(('center', 100))
-
     fact = cat.get_cat_fact()
-    factClip = TextClip(txt=fact, font="Amiri-Bold", size=(900, 800), fontsize=100, color="black",
-                        method='caption').set_duration(clip.duration)
 
     voice = cat.get_fact_audio(fact)
     voiceClip = AudioFileClip(voice)
     voiceClip = volumex(voiceClip, 3)
 
+    textClip = TextClip(txt=f'Cat Facts #{COUNTER}', font="Amiri-Bold", fontsize=150, color="black").set_duration(
+        voiceClip.duration)
+    textClip = textClip.set_position(('center', 100))
+
+    clip = ColorClip(size=(1080, 1920), color=[255, 255, 255], duration=voiceClip.duration)
+
+    # audioClip = AudioFileClip("audio/Cat Circus - Doug Maxwell.mp3").set_duration(clip.duration)
+    # audioClip = volumex(audioClip, 0.1)
+
+    gif = cat.get_cat_gif()
+    # gif = 'temp/img.gif'
+    gifClip = VideoFileClip(gif).set_position(('center', 350))
+    loopGifClip = loop.loop(gifClip, duration=clip.duration)
+    bigGifClip = resize.resize(loopGifClip, (gifClip.w * 2, gifClip.h * 2))
+
     y = 350 + bigGifClip.h
+
+    factClip = TextClip(txt=fact, font="Amiri-Bold", size=(900, 800), fontsize=75, color="black",
+                        method='caption').set_duration(voiceClip.duration)
 
     factClip = factClip.set_position(('center', y))
 
     video = CompositeVideoClip([clip, bigGifClip, textClip, factClip])
-    new_audioclip = CompositeAudioClip([audioClip, voiceClip])
+    new_audioclip = CompositeAudioClip([voiceClip])
     video.audio = new_audioclip
 
     video.write_videofile("movie.mp4", codec='libx264',
                           audio_codec='aac')
 
-    upload.upload_blob()
+    # upload.upload_blob()
+
