f550577ed3a23ff52b8867c93bf02582834969f4
rubylovesbread <admin@batatapenguin.com>
2024-08-08 07:13:19 +0000

    add files

--- app.py
+++ app.py
@@ -6,9 +6,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    main()
     print('success')
-    return render_template('index.html')
+    return render_template('index.html', filename='sample_movie.mp4')
+
+
+@app.route('/create')
+def create():
+    main()
+    return render_template('index.html', filename='movie.mp4')
 
 
 if __name__ == ' __main__':
--- helpers/cat.py
+++ helpers/cat.py
@@ -1,20 +1,21 @@
 import requests
 from helpers import genaudio
 
-CAT_FACT_URL = 'https://cat-fact.herokuapp.com/facts'
-CAT_GIF_URL = 'https://cataas.com/cat/gif'
+CAT_FACT_URL = 'https://catfact.ninja/fact'
+CAT_GIF_URL = 'https://g.tenor.com/v1/random?q=cat&key=LIVDSRZULELA&limit=1'
 
 
 def get_cat_fact():
-    res = requests.get(CAT_FACT_URL).json()[0]
-    output = res['text']
+    res = requests.get(CAT_FACT_URL).json()
+    output = res['fact']
     print(output)
     return output
 
 
 def get_cat_gif():
     print('get cat gif')
-    data = requests.get(CAT_GIF_URL).content
+    res = requests.get(CAT_GIF_URL).json()['results'][0]['media'][0]['gif']
+    data = requests.get(res['url']).content
     f = open('temp/img.gif', 'wb')
     f.write(data)
     f.close()
@@ -22,6 +23,6 @@ def get_cat_gif():
 
 
 def get_fact_audio(text):
-    #path = genaudio.generate_speech(text)
-    path = "audio/factAudio.wav"
+    path = genaudio.generate_speech(text)
+    #path = "audio/factAudio.wav"
     return path
